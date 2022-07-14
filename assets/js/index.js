e542ea823402151155165dff56466ab67c4f1c77
Roger Matamoros <supermixpc@outlook.com>
2022-07-14 18:50:16 -0500

    final

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,11 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/js/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
 var typed = new Typed("#text-slider", {
     strings: ["","Backend", "Frontend", "Fullstack"],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
 })
+
