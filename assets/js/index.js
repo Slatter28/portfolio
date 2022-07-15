e2f341f5f895258133821d023a8baab9a32e3ad3
Roger Matamoros <supermixpc@outlook.com>
2022-07-14 20:27:46 -0500

    correo listo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,9 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+
+const menu = document.getElementById('navbar__menu')
+const abrir = document.getElementById('navbar__btn')
+const cerrar = document.getElementById('navbar__cerrar')
+const form = document.getElementById('form')
+
 particlesJS.load('particles-js', 'assets/js/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
@@ -9,3 +14,51 @@ var typed = new Typed("#text-slider", {
     loop: true
 })
 
+
+abrir.addEventListener('click', (e) => {
+  menu.classList.add('mostrar__menu')
+})
+
+cerrar.addEventListener('click', (e) => {
+  menu.classList.remove('mostrar__menu')
+})
+
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+
+  const email = 'devytechsolutions@gmail.com'
+  const URL_BASE = `https://formsubmit.co/${email}`
+
+  const input = e.currentTarget.elements
+  const formData = {
+    name: input.name.value,
+    email: input.email.value,
+    message: input.message.value
+  }
+
+  console.log(formData)
+  const options = {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(formData)
+  }
+
+  /* con then y catch */
+  fetch(URL_BASE, options)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+  /* Con async / await */
+  try {
+    const res = await fetch(URL_BASE, options)
+    const data = await res.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+})
